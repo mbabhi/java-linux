1da3b7ffb6a046e3917c96fef16df8e2c82e437f
abhi <mbabhi92@gmail.com>
2020-06-04 01:05:32 +0200

    "EDITING BRANCH"

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Fuck Corona!");
+		System.out.println("GO Corona TO V01!");
 	}
 }
